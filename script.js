4287d7f9ffd98d8f6357bb1e1269664e41bcceb0
Ahammed Shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2022-07-06 18:41:46 +0530

    made a single time play game

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
-console.log("connected");
+//making rock  paper scissors selection of computer 
+function computerPlay() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerSelection;
+
+  if (randomNumber === 1) {
+    computerSelection = "ROCK";
+  } else if (randomNumber === 2) {
+    computerSelection = "PAPER";
+  } else {
+    computerSelection = "SCISSORS";
+  }
+  return computerSelection;
+}
+
+let computerSelection = computerPlay();
+let playerSelection = prompt("Choose Rock Paper or Scissors").toUpperCase();
+console.log(computerSelection);
+console.log(playerSelection);
+
+//playing a round of game
+function playRound() {
+  let result;
+  if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
+    result = "You Lose! Rock beats Scissors";
+  } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
+    result =  "You Lose! Paper beats Rock";
+  } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
+    result = "You Lose! Scissors beats Paper";
+  } else {
+    result = "You Wins";
+  }
+  return result;
+}
+
+function game() {
+  let result;
+  for(let i = 0; i < 5; i++){
+    result = playRound();
+  }
+  alert(result);
+}
+
+
 
